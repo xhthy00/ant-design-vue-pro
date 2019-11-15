986545f014d93b89be288c352f6b18c673e421e4
HYTong <xhthy00@qq.com>
2019-11-15 12:43:13 +0800

    配置less

--- antdpro/src/components/HelloWorld.vue
+++ antdpro/src/components/HelloWorld.vue
@@ -4,8 +4,8 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
+      <a-button href="https://cli.vuejs.org" target="_blank" rel="noopener"
+        >vue-cli documentation</a-button
       >.
     </p>
     <h3>Installed CLI Plugins</h3>
